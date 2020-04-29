a0b99cb8532d26ad935cbdd15da0549bd7f755b9
ravyoli314 <ravyoli314@gmail.com>
2020-04-29 23:27:15 +0200

    Corregido scanf. Volcado de las operaciones en array de elementos

--- costCalculator.c
+++ costCalculator.c
@@ -20,91 +20,83 @@
  * @param argv
  * @return
  */
-int main (int argc, const char * argv[] ) {
+int main (int argc, const char * argv[] ) { // ./calculator <file_name> <num. Producers> <buff. Size>
 
     if(argc < 4)
 	{
 		printf("No se han pasado suficientes argumentos\n");
 		return -1;
 	}
+    
     int total = 0;
     int numeroProductores = atoi(argv[2]);
-    printf("Total: %i €.\n", total);
-
-    // ./calculator <file_name> <num. Producers> <buff. Size>
-
-    FILE *id_fichero;
-    id_fichero = fopen(argv[1], "r");
+    FILE *id_fichero = fopen(argv[1], "r");
     
-    /* // id_fichero no es un entero (es una estructura)
-    if(id_fichero < 0){
+    if(id_fichero == NULL){
 		perror("error en la apertura del fichero especificado");
 		exit(-1);
-	}*/
+	}
 
-    // INSERTAR DATOS (operaciones) en el array de elementos
+    // INSERTAR DATOS (operaciones) en un array de elementos
 
-    char *lineas; // = Obtener la primera linea del fichero (scanf)
-    fscanf(id_fichero, "%s", lineas);
-    int numeroLineas = atoi(lineas);
-    printf("Líneas: %d \n", numeroLineas);
+    int numeroOperaciones; 
+    fscanf(id_fichero, "%d", &numeroOperaciones);
 
     // array de elementos con TODAS las operaciones (lo utilizarán los productores para pasar los datos al buffer circular)
-    element *arrayOperaciones = malloc(numeroLineas * sizeof(element)); 
-    printf("Acabo de reservar espacio \n"); 
-    free(arrayOperaciones);
+    element *arrayOperaciones = malloc(numeroOperaciones * sizeof(element)); 
 
-/*
-    for(int i = 0; i < numeroLineas; i++) { // insertar cada linea
+    for(int i = 0; i < numeroOperaciones; i++) { // insertar cada linea
         element nuevaOperacion;
-        char *indice;
-        char *tipo;
-        char *tiempo;
-        // char *espacio;
-        fscanf(id_fichero, "%s", indice);
-        fscanf(id_fichero, "%s", tipo);
-        fscanf(id_fichero, "%s", tiempo);
-        nuevaOperacion.type = atoi(tipo);
-        nuevaOperacion.time = atoi(tiempo);
+        int indice;
+        int tipo;
+        int tiempo;
+        fscanf(id_fichero, "%d", &indice);
+        fscanf(id_fichero, "%d", &nuevaOperacion.type);
+        fscanf(id_fichero, "%d", &nuevaOperacion.time);
         arrayOperaciones[i] = nuevaOperacion;
-
-//        int indiceNum = atoi(indice);
-        printf("Acabo de reservar fila %d \n", i);
     } 
 
     fclose(id_fichero);
 
-    for(int i = 0; i < numeroLineas; i++) { // PRINT
-        printf("%d %d \n", arrayOperaciones[i].time, arrayOperaciones[i].type);
+    for(int i = 0; i < numeroOperaciones; i++) { // PRINT
+        printf("%d %d \n", arrayOperaciones[i].type, arrayOperaciones[i].time);
     } 
 
     free(arrayOperaciones);
 
-*/
 
 
-/*
+/* ----------- CREAR BUFFER CIRCULAR (reservar el espacio especificado) ---------------------
     queue bufferCircular;
     bufferCircular.length = argv[3];
     bufferCircular.arrayElementos = malloc(bufferCircular.length * sizeof(struct element));
     
 */
-/*
+/* ------------ CREAR HILOS, SEMÁFOROS Y VARIABLES CONDICIONALES------------------------------
+// A los hilos hay que pasarles el rango de operaciones a insertar en el buffer, y la dirección de inicio del buffer (?). 
+    
     while(1 -> numProd){
         pthread_create(&th1, null, productor, null);
     }
     pthread_create(&th1, null, consumidor, null);
+
+// ESPERAR A QUE TERMINEN TODOS LOS HILOS Y DESTRUIR LOS SEMAFOROS Y VARIABLES CONDICIONALES
+// Recoger la suma total del hilo consumidor (la tiene que devolver)
 
     pthread_join();
     ....
 
     pthread_mutex_destroy()
     destroy variables consumidor
-
-free(); // libear fich
 */
+
+    printf("Total: %i €.\n", total);
     return 0;
 }
+
+
+
+// ---------- CREAR LAS FUNCIONES PRODUCTOR Y CONSUMIDOR (y definirlas en queue.h) ------------
 
 /*
 void productor(int inicio, int fin){
